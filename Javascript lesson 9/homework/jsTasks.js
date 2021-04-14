6b54f9c0e6a2e8be3e31918450dca2fdfdb33bd8
Łukasz Szafran <szafran@interia.pl>
2021-04-14 13:49:16 +0200

    homework5

---
+++ Javascript lesson 9/homework/jsTasks.js
@@ -0,0 +1,195 @@
+// task 1
+// a)
+let name = "Karol";
+// b)
+let age = 40;
+// c)
+let isOpen = true;
+// d)
+let stringBoolean = "true";
+let stringNumber = "100";
+
+// task 2
+// a)
+console.log("Michał");
+// b)
+console.log("Hello Ania");
+// c)
+console.log(4);
+// d
+console.log(false);
+
+// task 3
+// a)
+console.log(name);
+// b)
+console.log(age);
+// c)
+console.log(isOpen);
+// d)
+console.log(stringBoolean);
+// e)
+console.log(stringNumber);
+
+// task 4
+// a)
+console.log(2 + 2);
+// b)
+console.log("Michał " + "Kowalski");
+// c)
+console.log(2 + 2 * 2);
+
+// task 5
+let number = 4 + 6;
+console.log("wynik", number);
+
+// task 6
+let colour = "green";
+colour = "black";
+console.log(colour);
+
+// task 7
+// a)
+function add() {
+  return 2 + 2;
+}
+// b)
+function greeting() {
+  return "Hello World!";
+}
+// c)
+function form() {
+  return true;
+}
+// d)
+function modulo(a, b) {
+  return 6 % 4;
+}
+// e)
+function power(a, b) {
+  return 3 ** 4;
+}
+// f)
+function Hey() {
+  return "Hello, " + "how are you?";
+}
+// g)
+function multi() {
+  return 5 * 60;
+}
+// h)
+function multiple() {
+  return 3 * 3600;
+}
+// i)
+function calculate() {
+  return (10 * 9) / 2;
+}
+// j)
+function minus() {
+  return 200 - 10;
+}
+
+// task 8
+// a)
+let nr = [1, 2, 3];
+// b)
+let cars = ["BMW", "Audi", "Mercedes-Benz", "Toyota", "Honda"];
+// c)
+let options = [true, false, true];
+// d)
+let nothing = [];
+// e)
+let mixed = ["Marcin", false, 123456, "red", true];
+// f)
+console.log(mixed[1]);
+// g)
+let days = ["Poniedizałek", "Wtorek", "Środa"];
+console.log(days["length"]);
+// h)
+let none = [];
+none.push("apple");
+console.log(none);
+
+// task 9
+// a)
+let obj = {};
+// b)
+let person = {
+  name: "Iza",
+};
+// c)
+let personDetails = {
+  name: "Jarosław",
+  lastName: "Kaczyński",
+  age: 2,
+  drivingLicence: false,
+};
+// d)
+console.log(personDetails["age"]);
+// e)
+console.log(personDetails);
+
+// task 10
+// a)
+function add2(a, b) {
+  return a + b;
+}
+// b)
+function string(napis) {
+  return napis;
+}
+// c)
+function choose(value) {
+  return value;
+}
+// d)
+function moduloSecond(nr1, nr2) {
+  return nr1 % nr2;
+}
+// e)
+function power2(a, b) {
+  return a ** b;
+}
+// f)
+function stringSecond(word1, word2) {
+  return word1 + word2;
+}
+// g)
+function timer1(minutes) {
+  return minutes * 60;
+}
+// h)
+function timer2(hours) {
+  return hours * 60 * 60;
+}
+// i)
+function triangle(a, b) {
+  return a * b * 0, 5;
+}
+// j)
+function howManyDays(years) {
+  return years * 365;
+}
+// k)
+function power3(a) {
+  return a ** a;
+}
+// l)
+function triangle2(side1, side2) {
+  return side1 + side2 - 1;
+}
+// m)
+function hey2(name) {
+  return "Cześć, " + name + "!";
+}
+// n)
+function age(bornYear) {
+  return 2021 - bornYear;
+}
+// o)
+function sumOfPoints(a, b) {
+  return a * 2 + b * 3;
+}
+
+// Zadanie 3 pkt a) to "name" jakby było const ale nie wiem gdzie o-O
